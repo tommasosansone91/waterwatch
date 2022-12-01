4e387fd040a05dca3201878c8b5b512cfc58ec55
tommasosansone91 <tommaso.sansone91@virgilio.it>
2022-12-02 00:13:37 +0100

    addedstyles and settings of django-geojson

--- waterwatch/waterwatch/settings.py
+++ waterwatch/waterwatch/settings.py
@@ -12,9 +12,13 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 
+# added
+import os
+import posixpath
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+# BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
@@ -39,7 +43,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'django.contrib.gis',  # gis app
     'leaflet',
-    'waterwatchapp'  # my app
+    'waterwatchapp',  # my app
+    'djgeojson',
 ]
 
 MIDDLEWARE = [
@@ -82,7 +87,7 @@ DATABASES = {
     #     'NAME': BASE_DIR / 'db.sqlite3',
     # }
 
-    'default': {
+    'default': {  # changed
         'ENGINE': 'django.contrib.gis.db.backends.postgis',
         'NAME': 'cptwater',
         'USER': 'waterwatch',
@@ -92,6 +97,9 @@ DATABASES = {
 
 }
 
+SERIALIZATION_MODULES = {  # added
+    'geojson': 'djgeogeson.serializer'
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
@@ -128,6 +136,25 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# change made by the teacher but not recommended with django's new syntax
+# STATIC_ROOT = posixpath.join(*(BASE_DIR.split(os.path.sep) + ['static']))
+
+LEAFLET_CONFIG = {
+    'DEFAULT_CENTER': (-33.925, 18.625),
+    'DEFAULT_ZOOM': 10,
+    'MAX_ZOOM': 20,
+    'MIN_ZOOM': 3,
+    'SCALE':'both',
+    'ATTRIBUTION_PREFIX':'Powered by EBISYS',
+    'TILES': [('Open Street Map', 'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {'attribution': '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>', 'maxZoom': 20}),
+          ('Terrain Map', 'http://services.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {'attribution': 'Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community', 'maxZoom': 20}),
+          ('Data Map', 'https://cartodb-basemaps-{s}.global.ssl.fastly.net/dark_all/{z}/{x}/{y}.png', {'attribution':'&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> &copy; <a href="http://cartodb.com/attributions">CartoDB</a>', 'maxZoom': 20})],
+    'OVERLAYS': [('Suburbs', 'https://citymaps.capetown.gov.za/agsext1/rest/services/Theme_Based/EGISViewer/MapServer/76/query?outFields=*&where=1%3D1', {'attribution': '&copy; IGN'}),
+                 ('Dams', 'http://server/a/{z}/{x}/{y}.png', {'attribution': '&copy; IGN'})]
+
+}
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field

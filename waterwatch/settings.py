735bd85abea84fb4061c6e39c6481eb85821752e
tommasosansone91 <tommaso.sansone91@virgilio.it>
2024-06-23 03:02:14 +0200

    changed order of static files vars

--- waterwatch/settings.py
+++ waterwatch/settings.py
@@ -148,17 +148,17 @@ USE_TZ = False
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-# change made by the teacher but not recommended with django's new syntax
-# STATIC_ROOT = posixpath.join(*(BASE_DIR.split(os.path.sep) + ['static']))
-# /home/tommaso/tommaso03/coding_projects/corsi_udemy/create-smart-maps-in-python-and-leaflet/waterwatch/static/
-
 # my add
 # Custom static files (images, javascript and css - i.e. everything useful for beautifying the frontend) 
 # are developed in the static folders defined in the list variable `STATICFILES_DIRS` in settings.
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+# change made by the teacher but not recommended with django's new syntax
+# STATIC_ROOT = posixpath.join(*(BASE_DIR.split(os.path.sep) + ['static']))
+# /home/tommaso/tommaso03/coding_projects/corsi_udemy/create-smart-maps-in-python-and-leaflet/waterwatch/static/
 
 STATICSTORAGE = "Whitenoise.storage.CompressedManifestStaticFilesStorage" #zips up static files
 
